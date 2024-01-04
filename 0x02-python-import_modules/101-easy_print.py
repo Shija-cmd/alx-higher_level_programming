ce3fcb81fd45a73da4b06f890d16634cf3aa2a06
Shija-cmd <shijajuma@protonmail.com>
2024-01-04 18:44:01 +0000

    Adding 101-easy_print.py file

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Import function
+
+Auth: Juma Shija
+"""
+import easy_print
---
+++ 0x02-python-import_modules/easy_print.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Printing "pythoniscool"
+
+Auth: Juma Shija
+"""
+print("#pythoniscool")
