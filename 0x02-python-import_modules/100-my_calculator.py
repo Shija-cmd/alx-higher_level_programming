e9dfcf05aa38326579f809464ef3bd71fb3d7331
Shija-cmd <shijajuma@protonmail.com>
2024-01-04 18:34:50 +0000

    Adding 100-my_calculator.py file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Importing sys and operations functions
+
+Auth: Juma Shija
+"""
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv) - 1
+    if n != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    operation = sys.argv[2]
+    if operation != '+' and operation != '-' and operation != '*' and operation != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if operation == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operation == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operation == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
