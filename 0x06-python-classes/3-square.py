e51e3764caf9cb6616aff265f058528d41e8236b
Shija-cmd <shijajuma@protonmail.com>
2024-01-24 20:44:32 +0000

    Update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" class Square that defines a square by: (based on 2-square.py) """
 Square = __import__('3-square').Square
 
+""" Describe instance my_square_1 """
 my_square_1 = Square(3)
 print("Area: {}".format(my_square_1.area()))
 
+""" Private instance attribute: size """
 try:
     print(my_square_1.size)
 except Exception as e:
@@ -14,5 +17,6 @@ try:
 except Exception as e:
     print(e)
 
+""" Describe instance my_square_2 """ 
 my_square_2 = Square(5)
 print("Area: {}".format(my_square_2.area()))
