72bca7b37b57f561bd6f2c118d4472df65017ff7
Shija-cmd <shijajuma@protonmail.com>
2024-01-24 20:23:15 +0000

    Adding 1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Describes the class Square"""
+
+
+class Square:
+    """
+    Class that describes the Square
+    """
+    def __init__(self, size):
+        """Creates new instances of square class
+        """
+        self.__size = size
