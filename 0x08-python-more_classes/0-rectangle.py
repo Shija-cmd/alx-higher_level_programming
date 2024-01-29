07f3aec76f03d11b325308a2141d47a60789a478
Shija-cmd <shijajuma@protonmail.com>
2024-01-29 19:48:18 +0000

    update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Describes an empty class Rectangle"""
 
+
 class Rectangle:
-    """Class that defines properties of rectangle. """
+    """Class that describes properties of rectangle.
+	"""
     pass
