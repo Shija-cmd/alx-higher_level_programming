873cf17c8fbf256118a20facdf64de87211f689b
Shija-cmd <shijajuma@protonmail.com>
2024-02-06 22:07:37 +0000

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""describes function to write string to text file"""
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="UTF-8") as f:
+    """writes strings to text file"""
+    with open(filename, 'w') as f:
         return f.write(text)
