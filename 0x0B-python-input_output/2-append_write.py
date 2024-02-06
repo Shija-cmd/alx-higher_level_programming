f4d85d01578f45631bc602ea774edc8fe3bf7d31
Shija-cmd <shijajuma@protonmail.com>
2024-02-06 22:09:31 +0000

    update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Function to append a file with utf-8
-"""
+"""describes function to appends string to text file"""
+
 
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
+    """appends strings to text file"""
+    with open(filename, 'a') as f:
         return f.write(text)
