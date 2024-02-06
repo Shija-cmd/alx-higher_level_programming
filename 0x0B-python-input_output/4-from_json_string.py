18d9f321f3c4013ecb72d9fdc5858752b2214ee0
Shija-cmd <shijajuma@protonmail.com>
2024-02-06 21:14:09 +0000

    update

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Function to print json to object
+""" Importing json
 """
 import json
+""" Function to print from json to object
+"""
 
 def from_json_string(my_str):
     return json.loads(my_str)
