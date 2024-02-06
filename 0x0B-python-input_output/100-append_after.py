4e7ab752064f774f1580ffdd2775a4d0181a6f51
Shija-cmd <shijajuma@protonmail.com>
2024-02-06 22:35:32 +0000

    update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+"""describes function to append text after each line with given string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """appends text before each line with given string"""
+    txt = ""
+    with open(filename, 'r') as f:
+        f_line = f.readline()
+        while f_line != "":
+            txt += f_line
+            if search_string in f_line:
+                txt += new_string
+            f_line = f.readline()
+    with open(filename, 'w') as f:
+        f.write(txt)
