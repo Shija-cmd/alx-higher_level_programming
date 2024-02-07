07f5614b8bede04f6c40e8a71915a189ad6a98be
Shija-cmd <shijajuma@protonmail.com>
2024-02-07 15:05:46 +0000

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
 ===========================
-Class Mylist Module
+Module with class MyList
 ===========================
 """
 
 
 class MyList(list):
-    """method print_sorted in the Class MyList"""
+    """Class with method print_sorted"""
     pass
 
     def print_sorted(self):
-        """Method to sort a list"""
+        """Methot that sorted a list"""
 
         print(sorted(list(self)))
