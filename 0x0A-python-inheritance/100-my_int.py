670c4ec8e2f8068a8252e3b7c45a6875baeba0a3
Shija-cmd <shijajuma@protonmail.com>
2024-02-07 16:21:54 +0000

    update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,40 +1,17 @@
 #!/usr/bin/python3
 """
-=============================
-class Myint in module
-=============================
+class MyInt that inheriting from int
 """
 
 
 class MyInt(int):
-    """class that inherited from int"""
+    """
+        class MyInt that inheriting from int
+    """
+    def __eq__(self, x):
+        """Defines module eq"""
+        return not super().__eq__(x)
 
-    def __init__(self, my_int):
-        """for initializing a value my_int"""
-
-        self.my_int = my_int
-
-    @property
-    def my_int(self):
-        return self.__my_int
-
-    @my_int.setter
-    def my_int(self, my_int):
-        if type(my_int) is not int:
-            raise TypeError("my_int must be an integer")
-        else:
-            self.__my_int = my_int
-
-    def __eq__(self, other):
-        """equal method"""
-
-        if self.my_int == other:
-            return False
-        else:
-            return True
-
-    def __ne__(self, other):
-            if self.my_int != other:
-                return False
-            else:
-                return True
+    def __ne__(self, x):
+        """Defines module not eq"""
+        return not super().__ne__(x)
