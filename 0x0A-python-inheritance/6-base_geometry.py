ce196c45145733c1ff51e414421e5ff045c1daea
Shija-cmd <shijajuma@protonmail.com>
2024-02-07 15:43:19 +0000

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===================================
+class BaseGeometry with module
+===================================
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+
+    @classmethod
+    def area(self):
+        """method for calculating area"""
+
+        raise Exception("area() is not implemented")
