e2108a81b01f7a83a18e6a1f2a43f87872bf82e1
Shija-cmd <shijajuma@protonmail.com>
2024-04-12 18:51:45 +0000

    Adding 1-filter_states.py file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 from sys import argv
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1].startswith("N"):
+            print(row)
     cur.close()
     conn.close()
