17307f296612f021aec48c1e4abea7a839259614
Shija-cmd <shijajuma@protonmail.com>
2024-04-26 09:28:39 +0000

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,7 +4,6 @@ script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)
 """
 
-
 if __name__ == "__main__":
     import sys
     from urllib import request, error
