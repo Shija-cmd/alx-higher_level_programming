f46c4aeba6f2a5043a1829ee89c65df9df43c267
Shija-cmd <shijajuma@protonmail.com>
2024-04-26 09:39:18 +0000

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,17 +5,16 @@ and displays the body of the response (decoded in utf-8)
 """
 
 import sys
-import urllib.parse
+import urllib.error
 import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
-    except error.HTTPError as er:
-         print('Error code:', er.code)
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
