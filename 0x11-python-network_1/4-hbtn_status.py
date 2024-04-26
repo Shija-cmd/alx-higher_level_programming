b718d703fa86217178b2af18a7503d46a8234be0
Shija-cmd <shijajuma@protonmail.com>
2024-04-26 20:12:20 +0000

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,10 +4,11 @@ script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
